67617fac3455b9e966ada7f3b94ff0eb74aa91ea
SpamDroidPrime <dkpatel1037@gmail.com>
2026-05-18 14:26:21 -0400

    Add files via upload

--- Claw_Claw.cpp
+++ Claw_Claw.cpp
@@ -13,34 +13,135 @@ motor ClawMotor = motor(PORT2, false);
 motor ClawMotor2 = motor(PORT4, false);
 
 controller Controller = controller();
+optical ColourSensor =  optical(PORT2);
 
-int main()
-{
+
+void DriveForward(int Speed, int timeMS){
+LeftMotor.spin(forward, Speed, percent);
+RightMotor.spin(forward, Speed, percent);
+
+wait(timeMS, msec);
+
+LeftMotor.stop(brakeType::hold);
+RightMotor.stop(brakeType::hold);
+
+}
+ void DriveBackward(int Speed, int timeMS){
+
+LeftMotor.spin(reverse, Speed, percent);
+RightMotor.spin(reverse, Speed, percent);
+
+wait(timeMS, msec);
+
+LeftMotor.stop(brakeType::hold);
+RightMotor.stop(brakeType::hold);
+
+}
+void turnLeft(int Speed, int timeMS){
+LeftMotor.spin(reverse, Speed, percent);
+RightMotor.spin(forward, Speed, percent);
+
+wait(timeMS, msec);
+
+LeftMotor.stop(brakeType::hold);
+RightMotor.stop(brakeType::hold);
+
+}
+
+void turnRight(int Speed, int timeMS){
+LeftMotor.spin(forward, Speed, percent);
+RightMotor.spin(reverse, Speed, percent);
+
+wait(timeMS, msec);
+
+LeftMotor.stop(brakeType::hold);
+RightMotor.stop(brakeType::hold);
+
+}
+
+void moveClawArmUp(int Speed, int timeMS){
+
+ClawMotor2.spin(forward, Speed, percent);
+
+wait(timeMS, msec);
+
+ClawMotor2.stop(brakeType::hold); 
+
+}
+void moveClawArmDown(int Speed, int timeMS){
+
+ClawMotor2.spin(reverse, Speed, percent);
+
+wait(timeMS, msec);
+
+ClawMotor2.stop(brakeType::hold);
+
+}
+void moveClawOpen(int Speed, int timeMS){
+
+ClawMotor.spin(reverse, Speed, percent);
+
+wait(timeMS, msec);
+
+ClawMotor.stop(brakeType::hold);
+
+}
+void moveClawClose(int Speed, int timeMS){
+
+ClawMotor.spin(forward, Speed, percent);
+
+wait(timeMS, msec);
+
+ClawMotor.stop(brakeType::hold);
+
+}
+
+void runAuton(){
+ColourSensor.setLightPower(75, percent);
+ColourSensor.setLight(ledState::on);
+
+    moveClawOpen(50, 100);
+
+    DriveForward(60, 700);
+    turnLeft(65, 550);
+
+    DriveForward(50, 700);
+    if (ColourSensor.color() == color::green){
+        Brain.Screen.print("Hello");
+     moveClawClose(50, 500);
+     DriveBackward(50, 100);
+     moveClawArmUp(50, 400);
+     DriveForward(50, 100);
+     moveClawOpen(50, 100);
+    }
+}
+
+void Driver(){
     float deadband = 3.0;
-    float turning = 0.65;
+    float turning = 1;
     while (true)
     {
         if (Controller.ButtonR1.pressing())
     {
-        ClawMotor.spin(forward, 100, percent);
+        ClawMotor.spin(forward, 50, percent);
         Brain.Screen.print("Pressed");
     }
     else if (Controller.ButtonR2.pressing())
     {
-        ClawMotor.spin(reverse, 100, percent);
+        ClawMotor.spin(reverse, 60, percent);
     }
     else 
     {
-        ClawMotor.stop(brakeType::coast); 
+        ClawMotor.stop(brakeType::hold); 
     }
 
     if (Controller.ButtonL1.pressing())
     {
-        ClawMotor2.spin(forward, 100, percent);
+        ClawMotor2.spin(forward, 60, percent);
     }
     else if (Controller.ButtonL2.pressing())
     {
-        ClawMotor2.spin(reverse, 100, percent);
+        ClawMotor2.spin(reverse, 60, percent);
     }
     else
     {
@@ -62,6 +163,9 @@ int main()
 
              wait(10, msec);
         }
-    }
+}
     
-
+int main()
+{
+runAuton();
+}
